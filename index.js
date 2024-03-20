479f0a04c0f5f21ec06c21c125d00b4630be8439
Ian <ianmutugi91@gmail.com>
2024-03-20 07:31:45 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+
+
+
+
+
+
+let num3 = 19;
+
+let num4 = 5;
+
+let mod = num3 % num4;
+
+
+
+
+
+
+
+let numbers = [10, 5, 20, 15];
+
+let max = Math.max(...numbers);
